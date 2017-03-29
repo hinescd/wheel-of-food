678dbe8126f834d667aa6f055b0305ad9e3e5979
hinescd <hinescd@dukes.jmu.edu>
2017-03-29 18:39:31 -0400

    Made the wheel spin when loaded

--- src/main/resources/static/logic.js
+++ src/main/resources/static/logic.js
@@ -1,6 +1,9 @@
 var url = "search?latitude=38.431506999999996&longitude=-78.86769&term=food&radius=3218";
 var allBusinesses;
 var spinnerBusinesses;
+var displayNames;
+var angularVelocity;
+var angularAcceleration;
 var canvas;
 var context;
 var colors = ["#0080ff", "#00ff00", "#00ffff", "#ff0000", "#ff00ff"]
@@ -16,30 +19,57 @@ function search() {
   xmlHttp.send(null);
 }
 
+function drawSpinner(timestamp) {
+  context.clearRect(0, 0, 500, 500);
+  context.rotate(angularVelocity/60);
+  console.log("Doing stuff" + angularVelocity);
+  for(var i = 0; i < displayNames.length; i++) {
+    context.fillStyle = colors[i % colors.length];
+    context.beginPath();
+    context.moveTo(0, 0);
+    context.arc(0, 0, 240, -Math.PI/displayNames.length, Math.PI/displayNames.length);
+    context.lineTo(0, 0);
+    context.fill();
+    context.stroke();
+    
+    context.fillStyle = "#000000";
+    var name = displayNames[i];
+    var width = context.measureText(name).width;
+    context.fillText(name, 237 - width, 10);
+    angularVelocity = Math.max(0, angularVelocity + angularAcceleration/60);
+    context.rotate(2*Math.PI/displayNames.length);
+  }
+  var nextFrame = window.requestAnimationFrame(drawSpinner);
+  if(angularVelocity <= 0) {
+    window.cancelAnimationFrame(nextFrame);
+    console.log("Done");
+  }
+}
+
 function initSpinner() {
   canvas = document.getElementById("cvs");
   context = canvas.getContext("2d");
   context.translate(250, 250);
   context.font = "24px sans-serif";
   
-  for(var i = 0; i < spinnerBusinesses.length; i++) {
-    context.fillStyle = colors[i % colors.length];
-    context.beginPath();
-    context.moveTo(0, 0);
-    context.arc(0, 0, 240, -Math.PI/spinnerBusinesses.length, Math.PI/spinnerBusinesses.length);
-    context.lineTo(0, 0);
-    context.fill();
-    context.stroke();
-    
-    context.fillStyle = "#000000";
-    var name = spinnerBusinesses[i].name;
-    var width = context.measureText(name).width;
-    while(width > 202) {
-      name = name.substring(0, name.length - 4) + "...";
-      width = context.measureText(name).width;
+  if(spinnerBusinesses.length > 0) {
+    angularVelocity = 20;
+    angularAcceleration = -1;
+    displayNames = [];
+    for(var i = 0; i < spinnerBusinesses.length; i++) {
+      var name = spinnerBusinesses[i].name;
+      var width = context.measureText(name).width;
+      while(width > 202) {
+        name = name.substring(0, name.length - 4) + "...";
+        width = context.measureText(name).width;
+      }
+      displayNames[i] = name;
     }
-    context.fillText(name, 237 - width, 10);
-    context.rotate(2*Math.PI/spinnerBusinesses.length);
+    window.requestAnimationFrame(drawSpinner);
+  } else {
+    var errorText = "No matches found";
+    var width = context.measureText(errorText).width;
+    context.fillText(errorText, -width/2, 10);
   }
 }
 
